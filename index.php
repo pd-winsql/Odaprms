38497ba14b56e4be55e43aa2c14d13816a01ef56
winsql <ronjcorpuz11@gmail.com>
2026-03-25 13:04:57 +0800

    foooter done, small adjustments made in nav hover

--- index.php
+++ index.php
@@ -41,8 +41,10 @@
             </p>
         </section>
     </main>
-    <footer>
-        <p>&copy; 2020 Dr. Aprille Ventura Clinica Dental. All rights reserved.</p>
+    <footer class="footer">
+        <div class="footer-container">
+            <p>&copy; <script>document.write(new Date().getFullYear())</script> Dr. Aprille Ventura Clinica Dental. All rights reserved.</p>
+        </div>
     </footer>
 </body>
 </html>
