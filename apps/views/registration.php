b8a7e9cf2f766418f6b0e674b07f474dc3ba9a7a
winsql <ronjcorpuz11@gmail.com>
2026-04-10 21:35:17 +0800

    added questions in the registration form

--- apps/views/registration.php
+++ apps/views/registration.php
@@ -15,7 +15,7 @@
     </nav>
     <section id="registration">
         <div class="registration-container">
-            <h2>Patient Registration</h2>
+            <h2>Patient Information</h2>
             <div class="form-row">
                 <div class="form-group">
                     <label for="lastname">Last Name:</label>
@@ -92,6 +92,179 @@
                 </div>
             </div>
         </div>
+        <div class="registration-container">
+            <h2>Guardian Information</h2>
+            <div class="form-row">
+                <div class="form-group">
+                    <label for="guardian-lastname">Last Name:</label>
+                    <input type="text" id="guardian-lastname" name="guardian_lastname" required>
+                </div>
+                <div class="form-group">
+                    <label for="guardian-firstname">First Name:</label>
+                    <input type="text" id="guardian-firstname" name="guardian_firstname" required>
+                </div>
+                <div class="form-group">
+                    <label for="guardian-middlename">Middle Name:</label>
+                    <input type="text" id="guardian-middlename" name="guardian_middlename">
+                </div>
+                <div class="form-group">
+                    <label for="guardian-relationship">Relationship:</label>
+                    <div class="select-wrapper">
+                        <select id="guardian-relationship" name="guardian_relationship" required>
+                            <option value="">--Please choose an option--</option>
+                            <option value="parent">Parent</option>
+                            <option value="sibling">Sibling</option>
+                            <option value="relative">Relative</option>
+                            <option value="other">Other</option>
+                            <img class="select-caret" width="30" height="30" src="../../public/assets/dropdown.png" alt="down-squared"/>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="registration-container questionnaire">
+            <h2>Medical Questionnaire</h2>
+            <div class="form-row">
+                <div class="form-group question-group">
+                    <label for="q1">Are you in good health?</label>
+                    <div class="radio">
+                        <input type="radio" id="q1-yes" name="q1" value="yes" required>
+                        <label for="q1-yes">Yes</label>
+                        <input type="radio" id="q1-no" name="q1" value="no" required>
+                        <label for="q1-no">No</label>
+                    </div>
+                </div>
+            </div>
+            //TODO: add "If yes, specify" then add a field for input
+            <div class="form-row">
+                <div class="form-group question-group">
+                    <label for="q2">Are you under medical condition right now?</label>
+                    <div class="radio">
+                        <input type="radio" id="q2-yes" name="q2" value="yes" required>
+                        <label for="q2-yes">Yes</label>
+                        <input type="radio" id="q2-no" name="q2" value="no" required>
+                        <label for="q2-no">No</label>
+                    </div>
+                </div>
+            </div>
+            //TODO: add "If yes, specify" then add a field for input
+            <div class="form-row">
+                <div class="form-group question-group">
+                    <label for="q3">Have you ever had serious illness or surgical operation?</label>
+                    <div class="radio">
+                        <input type="radio" id="q3-yes" name="q3" value="yes" required>
+                        <label for="q3-yes">Yes</label>
+                        <input type="radio" id="q3-no" name="q3" value="no" required>
+                        <label for="q3-no">No</label>
+                    </div>
+                </div>
+            </div>
+            //TODO: add "If yes, specify" then add a field for input
+            <div class="form-row">
+                <div class="form-group question-group">
+                    <label for="q4">Have you ever been hospitalized?</label>
+                    <div class="radio">
+                        <input type="radio" id="q4-yes" name="q4" value="yes" required>
+                        <label for="q4-yes">Yes</label>
+                        <input type="radio" id="q4-no" name="q4" value="no" required>
+                        <label for="q4-no">No</label>
+                    </div>
+                </div>
+            </div>
+            //TODO: add "If yes, specify" then add a field for input
+            <div class="form-row">
+                <div class="form-group question-group">
+                    <label for="q5">Are you taking any medication?</label>
+                    <div class="radio">
+                        <input type="radio" id="q5-yes" name="q5" value="yes" required>
+                        <label for="q5-yes">Yes</label>
+                        <input type="radio" id="q5-no" name="q5" value="no" required>
+                        <label for="q5-no">No</label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group question-group">
+                    <label for="q6">Do you smoke?</label>
+                    <div class="radio">
+                        <input type="radio" id="q6-yes" name="q6" value="yes" required>
+                        <label for="q6-yes">Yes</label>
+                        <input type="radio" id="q6-no" name="q6" value="no" required>
+                        <label for="q6-no">No</label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group question-group">
+                    <label for="q7">Do you use alcohol?</label>
+                    <div class="radio">
+                        <input type="radio" id="q7-yes" name="q7" value="yes" required>
+                        <label for="q7-yes">Yes</label>
+                        <input type="radio" id="q7-no" name="q7" value="no" required>
+                        <label for="q7-no">No</label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group question-group">
+                    <label for="q8">Do you use drugs?</label>
+                    <div class="radio">
+                        <input type="radio" id="q8-yes" name="q8" value="yes" required>
+                        <label for="q8-yes">Yes</label>
+                        <input type="radio" id="q8-no" name="q8" value="no" required>
+                        <label for="q8-no">No</label>
+                    </div>
+                </div>
+            </div>
+            //TODO: add a field for input in others
+            <div class="form-row">
+                <div class="form-group question-group">
+                    <label for="q9">Are you allergic to any of the ff; Local Anesthetics, Latex, Penicillin, Aspirin Others:</label>
+                    <div class="radio">
+                        <input type="radio" id="q9-yes" name="q9" value="yes" required>
+                        <label for="q9-yes">Yes</label>
+                        <input type="radio" id="q9-no" name="q9" value="no" required>
+                        <label for="q9-no">No</label>
+                    </div>
+                </div>
+            </div>
+            <h3>For women only:</h3>
+            <div class="form-row">
+                <div class="form-group question-group">
+                    <label for="q10">Are you pregnant?</label>
+                    <div class="radio">
+                        <input type="radio" id="q10-yes" name="q10" value="yes" required>
+                        <label for="q10-yes">Yes</label>
+                        <input type="radio" id="q10-no" name="q10" value="no" required>
+                        <label for="q10-no">No</label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group question-group">
+                    <label for="q11">Are you nursing?</label>
+                    <div class="radio">
+                        <input type="radio" id="q11-yes" name="q11" value="yes" required>
+                        <label for="q11-yes">Yes</label>
+                        <input type="radio" id="q11-no" name="q11" value="no" required>
+                        <label for="q11-no">No</label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group question-group">
+                    <label for="q12">Are you taking birth control pills?</label>
+                    <div class="radio">
+                        <input type="radio" id="q12-yes" name="q12" value="yes" required>
+                        <label for="q12-yes">Yes</label>
+                        <input type="radio" id="q12-no" name="q12" value="no" required>
+                        <label for="q12-no">No</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+        //Todo: continue the form with the following conditions
+        //Todo: Create a digital contract form for the patient to sign before submitting the registration form
     </section>
 </body>
 </html>
