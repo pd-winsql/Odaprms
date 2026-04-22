7bdc44c70c139261ef1c412d90ced3f64f66c78b
winsql <ronjcorpuz11@gmail.com>
2026-04-22 21:37:32 +0800

    REFACTOR: styles.css

--- apps/views/login.php
+++ apps/views/login.php
@@ -1,18 +1,23 @@
+    
+    <div class="page">
+        <form action="registration-form.php" method="POST" class="login-form">
+            <img src="public/assets/logo.png" alt="Logo" class="logo-log">
+            <h2 class="section-title">Login</h2>
 
-    <form action="login.php" method="POST" class="login-form">
-        <img src="public/assets/logo.png" alt="Logo" class="logo">
-        <h2>Login to Your Account</h2>
-        <div class="form-group">
-            <label for="username">Username or Email</label>
-            <input type="text" id="username" name="username" required>
-        </div>
-        <div class="form-group">
-        <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <button type="submit" class="btn-primary">Login</button>
-        <div style="text-align:center; margin-top: 16px;">
-        <span>Don't have an account? </span>
-        <a href="apps/views/ventura_dental_form.php" onclick="closeModal('myModal');">Register</a>
-        </div>
-    </form>
+            <div class="form-group">
+                <label for="username">Username or Email</label>
+                <input type="text" id="username email" name="username" required>
+            </div>
+            
+            <div class="form-group"> 
+            <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+
+            <button type="submit" class="btn-primary">Login</button>
+            <div style="text-align:center; margin-top: 16px;">
+            <span>Don't have an account?</span>
+            <a href="apps/views/ventura_dental_form.php" onclick="closeModal('myModal');">Register</a>
+            </div>
+        </form>
+    </div>
