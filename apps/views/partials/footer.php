fb6cf1881900a10fa69ebe941aeff3650b25ab6f
winsql <ronjcorpuz11@gmail.com>
2026-04-23 14:55:57 +0800

    MINOR: about us section padding changes

--- apps/views/partials/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-        <div class="footer-container">
-            <p>&copy; <script>document.write(new Date().getFullYear())</script> Dr. Aprille Ventura Clinica Dental. All rights reserved.</p>
-        </div>
-    </footer>
