2826e3736d4ffab3ed41d007f1fd03d9956e3d9b
Winjemelron Corpuz <ronjcorpuz11@gmail.com>
2026-08-12 22:54:10 +0800

    delete database migration files

--- database/migrations/003_email_only_authentication.sql
+++
@@ -1,8 +0,0 @@
--- Remove usernames and use unique email addresses for every login.
--- Target database only: db-oaprms-system
-
-USE `db-oaprms-system`;
-
-ALTER TABLE users
-    DROP INDEX username,
-    DROP COLUMN username;
